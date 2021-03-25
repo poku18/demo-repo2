fab6bc6feed103438409b5df9a94f5d29001cbbc
Pragya Bhandari <pragya@Pragyas-MacBook-Pro.local>
2021-03-25 08:59:04 +0545

    lets see how to get it done

--- Shopping_list.c
+++ Shopping_list.c
@@ -6,22 +6,39 @@
 char** command_splitter(char *);
 
 struct items{
-    char category[20];
-    char item[20];
+    char *category;
+    char *item;
     int qty;
-    //struct items *next; //Linked List implementation
 };
+
+void process(struct items *,char **, int *);
 
 int main(){
     char commands[MAX];
     int i=0;
-    scanf("%[^\n]%*c", commands);
-    char** temp = command_splitter(commands);
-    
-    while(temp[i][0]!='\0'){
-        printf("%s\n",temp[i]);
-        i++;
+    int pos=0;
+    struct items *list=malloc(sizeof(struct items)*20);
+    while(1){
+        //Input of commands
+        scanf("%[^\n]%*c", commands);
+        char** temp = command_splitter(commands);
+        printf("%s\n",temp[0]);
+        //exist criteria/command
+        if(temp[0]=="exit"){
+            printf("anytime?");
+            break;
+        }
+        //process(list,temp,&pos);
+        //printing parts to remove
+        /*printf("%s\n",list[pos].item);
+        printf("%d\n",list[pos].qty);
+        printf("%s\n",list[pos].category);
+        */
+
+        free(temp);
+
     }
+    free(list);
     return 0;
 }
 char** command_splitter(char *command){
@@ -45,5 +62,18 @@ char** command_splitter(char *command){
     processed[word+1][0]='\0'; //adding a sentinel value to mark end.
     return processed;
 }
-//changes made here. 
+
+//Function to add items in array of struct
+void process(struct items *list,char **split_command, int *pos){
+    list[*pos].item=split_command[1];
+    list[*pos].category=split_command[3];
+    if(split_command[0]=="add"){
+        list[*pos].qty+= (int)split_command[2];
+    }
+    else if(split_command[0]=="add"){
+        list[*pos].qty-= (int)split_command[2];
+    }
+    *pos++;
+    return;
+}
 
